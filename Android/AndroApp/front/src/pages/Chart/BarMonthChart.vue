a41832cea97f81a3314e353b3e3661dc5f949c95
Lenoss <45673827+Lenoss@users.noreply.github.com>
2021-11-12 17:26:58 +0100

    ADD- app Android

---
+++ Android/AndroApp/front/src/pages/Chart/BarMonthChart.vue
@@ -0,0 +1,44 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  props: {
+    arrayWorkingTimes: Object
+  },
+
+  mounted() {
+    let chartData = [];
+    for (const [key, value] of Object.entries(this.arrayWorkingTimes)) {
+       chartData.push(value);
+    }
+
+    this.renderChart(
+      {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            label: "Hour per month",
+            backgroundColor: "#2dce89",
+            data: chartData,
+          },
+        ],
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  },
+};
+</script>
