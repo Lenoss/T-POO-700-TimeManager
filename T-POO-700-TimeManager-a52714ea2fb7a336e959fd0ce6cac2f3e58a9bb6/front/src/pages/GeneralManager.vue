bd79f4b45671c47e5c3e2c995e3b0765758d9690
Lenoss <45673827+Lenoss@users.noreply.github.com>
2021-11-15 00:02:59 +0100

    test

---
+++ T-POO-700-TimeManager-a52714ea2fb7a336e959fd0ce6cac2f3e58a9bb6/front/src/pages/GeneralManager.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h3 class="p-3 text-center">USER LIST</h3>
+    <div class="table-responsive">
+      <table class="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in filterUsers" :key="user.id">
+            <td>{{ user.firstname }} {{ user.lastname }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.role }}</td>
+            <td>
+              <button class="btn btn-success m-1">Promote</button>
+              <button class="btn btn-danger m-1">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import authHeader from "../services/auth-header";
+
+const apiEndPoint = process.env.VUE_APP_API_ENDPOINT;
+
+export default {
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    filterUsers: function () {
+      return this.users.filter(function (u) {
+        return u.role != "GeneralManager";
+      });
+    }, // contains only Alex and James
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    axios
+      .get(`${apiEndPoint}/users`, authHeader())
+      .then((response) => {
+        this.users = response.data.data;
+        console.log(this.users);
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+};
+</script>
+<style scope="">
+thead {
+  background-color: rgba(39, 41, 61, 1);
+}
+.table-bordered {
+  border: 0.0625rem solid rgba(255, 255, 255, 0.1);
+}
+.table-responsive {
+  overflow: auto;
+}
+.table {
+  width: 99% !important;
+}
+</style>
---
+++ T-POO-700-TimeManager-a52714ea2fb7a336e959fd0ce6cac2f3e58a9bb6/timemanager/lib/timemanager/data/user.ex
@@ -0,0 +1,39 @@
+defmodule Timemanager.Data.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Bcrypt
+
+  schema "users" do
+    field :address, :string
+    field :city, :string
+    field :country, :string
+    field :email, :string
+    field :firstname, :string
+    field :lastname, :string
+    field :role, :string
+    field :zipcode, :string
+    field :password_hash, :string
+
+    field :password, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:address, :city, :country, :email, :firstname, :lastname, :role, :zipcode, :password])
+    |> validate_required([:address, :city, :country, :email, :firstname, :lastname, :role, :zipcode])
+    |> put_password_hash
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}}
+        ->
+          put_change(changeset, :password_hash, hash_pwd_salt(pass))
+      _ ->
+          changeset
+    end
+  end
+end
